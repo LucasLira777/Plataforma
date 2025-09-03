43c3b202a1f5ed8c1e95e9f23372a5c1449e5350
LucasLira777 <lira.fire.2007@gmail.com>
2025-09-03 18:50:26 -0300

    correndo

--- Plataforma/Assets/Scripts/Player.cs
+++ Plataforma/Assets/Scripts/Player.cs
@@ -6,8 +6,10 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public Vector2 friction = new Vector2(.1f, 0);
-    public float speed = 15;
+    public float speed ;
+    public float speedRun;
     public float forceJump = 10;
+    private float _currentSpeed;
     
    private void Update()
     {
@@ -17,15 +19,21 @@ public class Player : MonoBehaviour
 
     private void HandleMoviment()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+            _currentSpeed = speedRun;
+        else
+            _currentSpeed = speed;
+
+
         if (Input.GetKey(KeyCode.A))
         {
             //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
-            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(-_currentSpeed, myRigidbody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
-            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
         }
 
         if (myRigidbody.velocity.x > 0)
